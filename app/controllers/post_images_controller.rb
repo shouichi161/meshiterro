7876edda7e95b2ca35c7576ace1487d5a6dcf269
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-02-18 10:05:09 +0000

    [finish]4-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,13 +2,13 @@ class PostImagesController < ApplicationController
   def new
     @post_image=PostImage.new
   end
-　
-　def create
-　  @post_image=PostImage.new(post_image_params)
-　  post_image.user_id=current_user.id
-　  post_image.save
-　  redirect_to post_images_path
-　end  
+  
+ def create
+   @post_image=PostImage.new(post_image_params)
+   @post_image.user_id=current_user.id
+   @post_image.save
+   redirect_to post_images_path
+ end  
 
   def index
   end
@@ -22,6 +22,6 @@ class PostImagesController < ApplicationController
   private
   
   def post_image_params
-    params_require(:post_image).permit(:shop_name,:image,:caption)
+    params.require(:post_image).permit(:shop_name,:image,:caption)
   end 
 end
